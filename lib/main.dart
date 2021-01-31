e2d1263c5b631e523f1822659b2f38ea94261cf4
Vang Huynh <vanghm@imt-soft.com>
2021-01-31 21:37:17 +0700

    Lesson 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,31 @@
-import 'package:Learning/car.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  // for(int i = 0; i < numbers.length; i++) {
-  //   print(numbers[i]);
-  // }
-  // numbers.forEach((element) {
-  //   print(element);
-  // });
-  // stringNumbers.forEach((element) {
-  //   print(element);
-  // });
-  // How to init an object?
-  // var myCar = Car(name: 'Mercedes-Maybach S-class Saloon');
-  // myCar.handleEvent = () {
-  //   print('Handle in main');
-  // };
-  // myCar.doSomething();
-  // myCar.sayHello('Vang');
-  // List of cars?
-  List<Car> cars = <Car>[
-    Car(name: 'Vinfast', yearOfProduction: 2021),
-    Car(name: 'Toyota', yearOfProduction: 2020),
-    Car(name: 'Inova', yearOfProduction: 2010),
-  ];
-  // How to sort this list, by yearOfProduction?
-  // cars.sort((car1, car2) => car1.yearOfProduction - car2.yearOfProduction);// sort ASC
-  // cars.sort((car1, car2) => car2.yearOfProduction - car1.yearOfProduction);// sort DESC
-  // How to sort by name?
-  cars.sort((car1, car2) => car1.name.compareTo(car2.name));
-  // Add an element
-  cars.add(Car(name: 'Honda'));
-  // how to filter?
-  var filtered = cars.where((element) => element.name.contains('Honda')).toList();
-  // how to delete?
-  cars = cars.where((element) => !element.name.contains('Honda')).toList();
+  runApp(MyApp(
+    age: 27,
+    name: "Vang",
+  ));
+}
 
-  // Now we try with final and conts(constant)
-  // final x = 10; // Can't set value for final and const(int, string, bool)
-  // x = 20;
+// You can define your own Widget
+class MyApp extends StatelessWidget {
+  String name;
+  int age;
 
-  final List<int> someNumbers = [1,2,3,5];
-  someNumbers.add(7);
-  
+  MyApp({this.name, this.age});
 
-  runApp(Center(
-      child: Text(
-    filtered.toString(),
-    textDirection: TextDirection.ltr,
-    style: TextStyle(fontSize: 30),
-  )));
+  @override
+  Widget build(BuildContext context) {
+    // Build: abstract method from parent class(StatelessWidget)
+    return MaterialApp(
+        title: "Learning",
+        home: Scaffold(
+          body: Center(
+              child: Text(
+            "Hello, my name $name, age: $age",
+            style: TextStyle(fontSize: 30),
+            textDirection: TextDirection.ltr,
+          )),
+        ));
+  }
 }
